9d16f1b1089054f0879ecf612ec6978c493d5769
talves <tony@3alves.com>
2022-08-15 12:07:05 -0700

    update: test

--- packages/@cmsonfire/cms-lib-template/src/lib/CmsonfireCmsLibTemplate.spec.tsx
+++ packages/@cmsonfire/cms-lib-template/src/lib/CmsonfireCmsLibTemplate.spec.tsx
@@ -19,4 +19,22 @@ describe('CmsonfireCmsLibTemplate', () => {
     );
     expect(baseElement).toBeTruthy();
   });
+  it('should render successfully: functional', () => {
+    const Widget = CmsonfireCmsLibTemplate.Widget();
+    const ControlWidget = Widget.controlComponent;
+    const PreviewWidget = Widget.previewComponent;
+    const { baseElement } = render(
+      <div>
+        <ControlWidget
+          forID="test"
+          onChange={(e) => e.target.value}
+          classNameWrapper=""
+          setActiveStyle={() => null}
+          setInactiveStyle={() => null}
+        />
+        <PreviewWidget value={<div>test</div>} />
+      </div>
+    );
+    expect(baseElement).toBeTruthy();
+  });
 });
